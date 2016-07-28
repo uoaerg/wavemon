01e987a032b81af7bbeba1c439759d750dc1f398
Jörg Krause <joerg.krause@embedded.rocks>
2016-07-28 21:24:33 +0200

    Fix musl build
    Add missing header `stdint.h` to fix build with the musl C library:
    
    ```
    wavemon.h:240:7: error: unknown type name ‘int8_t’
    ```

--- wavemon.h
+++ wavemon.h
@@ -18,6 +18,7 @@
  * with wavemon; see the file COPYING.  If not, write to the Free Software
  * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
