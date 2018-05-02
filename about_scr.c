bd7e9701b4f8a6930b35d5ff052fda94a24e2b97
Gerrit Renker <Gerrit.Renker@ctl.io>
2018-05-02 11:04:17 -0600

    About screen: remove timezone
    This is confusing - on my system the displayed timestamp is relative to
    the local timezone, not UTC.

--- about_scr.c
+++ about_scr.c
@@ -24,7 +24,7 @@ static WINDOW *w_about;
 
 static char *about_lines[] = {
 	"wavemon - status monitor for wireless network devices",
-	"version " PACKAGE_VERSION " (built " __DATE__ " " __TIME__ " UTC)",
+	"version " PACKAGE_VERSION " (built " __DATE__ " " __TIME__ ")",
 	"",
 	"original by jan morgenstern <jan@jm-music.de>",
 	"distributed under the GNU general public license v3",
