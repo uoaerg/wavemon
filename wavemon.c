cfb51666523380515483d5a97286da05dea183c8
bigfreak85 <bigfreak@web.de>
2018-12-24 11:51:42 +0100

    use_default_colors for Transparency

--- wavemon.c
+++ wavemon.c
@@ -157,28 +157,29 @@ int main(int argc, char *argv[])
 	check_geometry();
 
 	start_color();
-	init_pair(CP_STANDARD,	COLOR_WHITE,	COLOR_BLACK);
-	init_pair(CP_SCALEHI,	COLOR_RED,	COLOR_BLACK);
-	init_pair(CP_SCALEMID,	COLOR_YELLOW,	COLOR_BLACK);
-	init_pair(CP_SCALELOW,	COLOR_GREEN,	COLOR_BLACK);
-	init_pair(CP_WTITLE,	COLOR_CYAN,	COLOR_BLACK);
-	init_pair(CP_INACTIVE,	COLOR_CYAN,	COLOR_BLACK);
+	use_default_colors();
+	init_pair(CP_STANDARD,	COLOR_WHITE,	-1);
+	init_pair(CP_SCALEHI,	COLOR_RED,	-1);
+	init_pair(CP_SCALEMID,	COLOR_YELLOW,	-1);
+	init_pair(CP_SCALELOW,	COLOR_GREEN,	-1);
+	init_pair(CP_WTITLE,	COLOR_CYAN,	-1);
+	init_pair(CP_INACTIVE,	COLOR_CYAN,	-1);
 	init_pair(CP_ACTIVE,	COLOR_CYAN,	COLOR_BLUE);
 
-	init_pair(CP_STATSIG,	  COLOR_GREEN,	COLOR_BLACK);
-	init_pair(CP_STATNOISE,	  COLOR_RED,	COLOR_BLACK);
+	init_pair(CP_STATSIG,	  COLOR_GREEN,	-1);
+	init_pair(CP_STATNOISE,	  COLOR_RED,	-1);
 	init_pair(CP_STATSNR,	  COLOR_BLUE,	COLOR_BLUE);
-	init_pair(CP_STATBKG,	  COLOR_BLUE,	COLOR_BLACK);
+	init_pair(CP_STATBKG,	  COLOR_BLUE,	-1);
 	init_pair(CP_STATSIG_S,	  COLOR_GREEN,	COLOR_BLUE);
 	init_pair(CP_STATNOISE_S, COLOR_RED,	COLOR_BLUE);
 
-	init_pair(CP_PREF_NORMAL, COLOR_WHITE,	COLOR_BLACK);
+	init_pair(CP_PREF_NORMAL, COLOR_WHITE,	-1);
 	init_pair(CP_PREF_SELECT, COLOR_WHITE,	COLOR_BLUE);
-	init_pair(CP_PREF_ARROW,  COLOR_RED,	COLOR_BLACK);
+	init_pair(CP_PREF_ARROW,  COLOR_RED,	-1);
 
-	init_pair(CP_SCAN_CRYPT,  COLOR_RED,	COLOR_BLACK);
-	init_pair(CP_SCAN_UNENC,  COLOR_GREEN,	COLOR_BLACK);
-	init_pair(CP_SCAN_NON_AP, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(CP_SCAN_CRYPT,  COLOR_RED,	-1);
+	init_pair(CP_SCAN_UNENC,  COLOR_GREEN,	-1);
+	init_pair(CP_SCAN_NON_AP, COLOR_YELLOW, -1);
 
 	/* Override signal handlers installed during ncurses initialisation. */
 	xsignal(SIGCHLD, SIG_IGN);
